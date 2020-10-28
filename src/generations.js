4f660ce6fd94c1f696d8eedc52c0e7b77ff9bb29
Kyle Dadika <kdadika@gmail.com>
2020-10-28 14:58:27 -0400

    Added buttons for all generations

--- src/generations.js
+++ src/generations.js
@@ -1,12 +1,42 @@
 export const generations = [
-  {
-    gen: 'genOne',
-    limit: 151,
-    offset: 0,
-  },
-  {
-    gen: 'genTwo',
-    limit: 100,
-    offset: 151
-  }
+	{
+		gen: 'genOne',
+		limit: 151,
+		offset: 0,
+	},
+	{
+		gen: 'genTwo',
+		limit: 100,
+		offset: 151,
+	},
+	{
+		gen: 'genThree',
+		limit: 135,
+		offset: 251,
+	},
+	{
+		gen: 'genFour',
+		limit: 107,
+		offset: 386,
+	},
+	{
+		gen: 'genFive',
+		limit: 156,
+		offset: 493,
+	},
+	{
+		gen: 'genSix',
+		limit: 72,
+		offset: 649,
+	},
+	{
+		gen: 'genSeven',
+		limit: 88,
+		offset: 721,
+	},
+	{
+		gen: 'genEight',
+		limit: 84,
+		offset: 809,
+	},
 ];
