ac5f233500077c7cde489a0c94085eda71c209ce
Kyle Dadika <kdadika@gmail.com>
2020-10-29 15:08:41 -0400

    Converted indentation to spaces

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { createBrowserHistory } from 'history';
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router history={history}>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router history={history}>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
