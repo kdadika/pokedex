0234f02ca70775e74f7f02dd3927f1519c3ef3ab
Kyle Dadika <kdadika@gmail.com>
2020-10-30 13:34:40 -0400

    Changed prettier settings

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import {
-	AppBar,
-	Toolbar,
-	Grid,
-	Card,
-	CardMedia,
-	CardContent,
-	Typography, 
-	TextField,
+  AppBar,
+  Toolbar,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  TextField,
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
@@ -20,164 +20,164 @@ import axios from 'axios';
 import { PokedexStyles } from './styles/PokedexStyles';
 
 const Pokedex = () => {
-	const history = useHistory();
-	const useStyles = makeStyles(PokedexStyles);
-	const classes = useStyles();
-	const [pokemonData, setPokemonData] = useState({});
-	const [filter, setFilter] = useState(``);
-	const [limit, setLimit] = useState(151);
-	const [offset, setOffset] = useState(0);
+  const history = useHistory();
+  const useStyles = makeStyles(PokedexStyles);
+  const classes = useStyles();
+  const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState(``);
+  const [limit, setLimit] = useState(151);
+  const [offset, setOffset] = useState(0);
 
-	const handleSearchChange = (e) => {
-		setFilter(e.target.value);
-	};
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value);
+  };
 
-	const handleClick = (e) => {
-		const generation = e.target.id;
+  const handleClick = (e) => {
+    const generation = e.target.id;
 
-		if (generation) {
-			const clickedGen = generations.find(
-				(foundGen) => foundGen.gen === generation
-			);
+    if (generation) {
+      const clickedGen = generations.find(
+        (foundGen) => foundGen.gen === generation
+      );
 
-			setLimit(clickedGen.limit);
-			setOffset(clickedGen.offset);
-		}
-	};
+      setLimit(clickedGen.limit);
+      setOffset(clickedGen.offset);
+    }
+  };
 
-	const getPokemonData = (limit, offset) => {
-		axios
-			.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
-			.then((response) => {
-				const { data: { results } = {} } = response;
-				const newPokemonData = {};
-				const pokemonNumber = offset + 1;
+  const getPokemonData = (limit, offset) => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+      .then((response) => {
+        const { data: { results } = {} } = response;
+        const newPokemonData = {};
+        const pokemonNumber = offset + 1;
 
-				results.forEach((pokemon, index) => {
-					newPokemonData[index + pokemonNumber] = {
-						id: index + pokemonNumber,
-						name: pokemon.name,
-						sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-							index + pokemonNumber
-						}.png`,
-					};
-				});
-				setPokemonData(newPokemonData);
-			});
-	};
+        results.forEach((pokemon, index) => {
+          newPokemonData[index + pokemonNumber] = {
+            id: index + pokemonNumber,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              index + pokemonNumber
+            }.png`,
+          };
+        });
+        setPokemonData(newPokemonData);
+      });
+  };
 
-	useEffect(() => {
-		getPokemonData(limit, offset);
-	}, [limit, offset]);
+  useEffect(() => {
+    getPokemonData(limit, offset);
+  }, [limit, offset]);
 
-	const getPokemonCard = (pokemonId) => {
-		const { id, name, sprite } = pokemonData[pokemonId];
+  const getPokemonCard = (pokemonId) => {
+    const { id, name, sprite } = pokemonData[pokemonId];
 
-		return (
-			<Grid item xs={4} key={pokemonId}>
-				<Card onClick={() => history.push(`/${pokemonId}`)}>
-					<CardMedia
-						className={classes.cardMedia}
-						image={sprite}
-						style={{ width: '130px', height: '130px' }}
-				  />
-					<CardContent className={classes.cardContent}>
-						<Typography>{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
-					</CardContent>
-				</Card>
-			</Grid>
-		);
-	};
+    return (
+      <Grid item xs={4} key={pokemonId}>
+        <Card onClick={() => history.push(`/${pokemonId}`)}>
+          <CardMedia
+            className={classes.cardMedia}
+            image={sprite}
+            style={{ width: '130px', height: '130px' }}
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography>{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  };
 
-	return (
-		<>
-			<AppBar position="static">
-				<Toolbar>
-					<div className={classes.searchContainer}>
-						<SearchIcon className={classes.SearchIcon} />
-						<TextField
-							onChange={handleSearchChange}
-							className={classes.searchInput}
-							label="Pokemon"
-							variant="standard"
-						/>
-					</div>
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.SearchIcon} />
+            <TextField
+              onChange={handleSearchChange}
+              className={classes.searchInput}
+              label="Pokemon"
+              variant="standard"
+            />
+          </div>
 
-					<button
-						id="genOne"
-						onClick={handleClick}
-						className={classes.genButton}
-						// variant="contained"
-					>
-						Gen One
-					</button>
-					<button
-						id="genTwo"
-						onClick={handleClick}
-						className={classes.genButton}
-						// variant="contained"
-					>
-						Gen Two
-					</button>
-					<button
-						id="genThree"
-						onClick={handleClick}
-						className={classes.genButton}
-						// variant="contained"
-					>
-						Gen Three
-					</button>
-					<button
-						id="genFour"
-						onClick={handleClick}
-						className={classes.genButton}
-						// variant="contained"
-					>
-						Gen Four
-					</button>
-					<button
-						id="genFive"
-						onClick={handleClick}
-						className={classes.genButton}
-						// variant="contained"
-					>
-						Gen Five
-					</button>
-					<button
-						id="genSix"
-						onClick={handleClick}
-						className={classes.genButton}
-						// variant="contained"
-					>
-						Gen Six
-					</button>
-					<button
-						id="genSeven"
-						onClick={handleClick}
-						className={classes.genButton}
-						// variant="contained"
-					>
-						Gen Seven
-					</button>
-					<button
-						id="genEight"
-						onClick={handleClick}
-						className={classes.genButton}
-						// variant="contained"
-					>
-						Gen Eight
-					</button>
-				</Toolbar>
-			</AppBar>
-			<Grid container spacing={2} className={classes.pokedexContainer}>
-				{Object.keys(pokemonData).map(
-					(pokemonId) =>
-						pokemonData[pokemonId].name.includes(filter) &&
-						getPokemonCard(pokemonId)
-				)}
-			</Grid>
-		</>
-	);
+          <button
+            id="genOne"
+            onClick={handleClick}
+            className={classes.genButton}
+            // variant="contained"
+          >
+            Gen One
+          </button>
+          <button
+            id="genTwo"
+            onClick={handleClick}
+            className={classes.genButton}
+            // variant="contained"
+          >
+            Gen Two
+          </button>
+          <button
+            id="genThree"
+            onClick={handleClick}
+            className={classes.genButton}
+            // variant="contained"
+          >
+            Gen Three
+          </button>
+          <button
+            id="genFour"
+            onClick={handleClick}
+            className={classes.genButton}
+            // variant="contained"
+          >
+            Gen Four
+          </button>
+          <button
+            id="genFive"
+            onClick={handleClick}
+            className={classes.genButton}
+            // variant="contained"
+          >
+            Gen Five
+          </button>
+          <button
+            id="genSix"
+            onClick={handleClick}
+            className={classes.genButton}
+            // variant="contained"
+          >
+            Gen Six
+          </button>
+          <button
+            id="genSeven"
+            onClick={handleClick}
+            className={classes.genButton}
+            // variant="contained"
+          >
+            Gen Seven
+          </button>
+          <button
+            id="genEight"
+            onClick={handleClick}
+            className={classes.genButton}
+            // variant="contained"
+          >
+            Gen Eight
+          </button>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        {Object.keys(pokemonData).map(
+          (pokemonId) =>
+            pokemonData[pokemonId].name.includes(filter) &&
+            getPokemonCard(pokemonId)
+        )}
+      </Grid>
+    </>
+  );
 };
 
 export default Pokedex;
--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -5,74 +5,74 @@ import { toFirstCharUppercase } from './utils/constants';
 import axios from 'axios';
 
 const Pokemon = (props) => {
-	const { match } = props;
-	const { params } = match;
-	const { pokemonId } = params;
-	const history = useHistory();
-	const [pokemon, setPokemon] = useState();
+  const { match } = props;
+  const { params } = match;
+  const { pokemonId } = params;
+  const history = useHistory();
+  const [pokemon, setPokemon] = useState();
 
-	useEffect(() => {
-		axios
-			.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
-			.then((response) => {
-				const { data } = response;
-				setPokemon(data);
-			})
-			.catch((error) => {
-			  setPokemon(false);
-			});
-	}, [pokemonId]); 
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
+      .then((response) => {
+        const { data } = response;
+        setPokemon(data);
+      })
+      .catch((error) => {
+        setPokemon(false);
+      });
+  }, [pokemonId]);
 
-	const generatePokemon = (pokemon) => {
-		const { name, id, species, height, weight, types, sprites } = pokemon;
-		const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
-		const { front_default } = sprites;
+  const generatePokemon = (pokemon) => {
+    const { name, id, species, height, weight, types, sprites } = pokemon;
+    const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
+    const { front_default } = sprites;
 
-		const pokemonTypes = types.map((typeInfo) => {
-			const { type } = typeInfo;
-			const { name } = type;
-			return (
-				<Typography key={toFirstCharUppercase(name)}>
-					{' '}
-					{`${toFirstCharUppercase(name)}`}
-				</Typography>
-			);
-		});
-		return (
-			<>
-				<Typography variant="h1">
-					{`${id}.`} {toFirstCharUppercase(name)}
-					<img src={front_default} alt="pokemon img" />
-				</Typography>
-				<img
-					style={{ width: '300px', height: '300px' }}
-					src={fullImageUrl}
-					alt="pokemon img"
-				/>
-				<Typography variant="h3">Pokemon Info</Typography>
-				<Typography>
-					{'Species: '}
-					<Link href={species.url}>{toFirstCharUppercase(species.name)}</Link>
-				</Typography>
-				<Typography>Height: {height}</Typography>
-				<Typography>Weight: {weight}</Typography>
-				<Typography variant="h6">Types:</Typography>
-				{pokemonTypes}
-			</>
-		);
-	};
-	return (
-		<>
-			{pokemon === undefined && <CircularProgress />}
-			{pokemon !== undefined && pokemon && generatePokemon(pokemon)}
-			{pokemon === false && <Typography>Pokemon not found</Typography>}
-			{pokemon !== undefined && (
-				<Button variant="contained" onClick={() => history.push('/')}>
-					Back to Pokedex
-				</Button>
-			)}
-		</>
-	);
+    const pokemonTypes = types.map((typeInfo) => {
+      const { type } = typeInfo;
+      const { name } = type;
+      return (
+        <Typography key={toFirstCharUppercase(name)}>
+          {' '}
+          {`${toFirstCharUppercase(name)}`}
+        </Typography>
+      );
+    });
+    return (
+      <>
+        <Typography variant="h1">
+          {`${id}.`} {toFirstCharUppercase(name)}
+          <img src={front_default} alt="pokemon img" />
+        </Typography>
+        <img
+          style={{ width: '300px', height: '300px' }}
+          src={fullImageUrl}
+          alt="pokemon img"
+        />
+        <Typography variant="h3">Pokemon Info</Typography>
+        <Typography>
+          {'Species: '}
+          <Link href={species.url}>{toFirstCharUppercase(species.name)}</Link>
+        </Typography>
+        <Typography>Height: {height}</Typography>
+        <Typography>Weight: {weight}</Typography>
+        <Typography variant="h6">Types:</Typography>
+        {pokemonTypes}
+      </>
+    );
+  };
+  return (
+    <>
+      {pokemon === undefined && <CircularProgress />}
+      {pokemon !== undefined && pokemon && generatePokemon(pokemon)}
+      {pokemon === false && <Typography>Pokemon not found</Typography>}
+      {pokemon !== undefined && (
+        <Button variant="contained" onClick={() => history.push('/')}>
+          Back to Pokedex
+        </Button>
+      )}
+    </>
+  );
 };
 
 export default Pokemon;
